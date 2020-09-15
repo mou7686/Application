ab9946433beb40d55193b710986c17bc82a155b0
abcd <mouli.pal@outlook.com>
2020-09-15 23:13:43 +0530

    update

--- app1.py
+++ app1.py
@@ -1,12 +1,30 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(w):
     if w in data:
         return data[w]
+
+    elif len(get_close_matches(w,data.keys())) > 0:
+        yn = input("Did you mean {} instead? Enter Y if Yes,or N if No: " .format(get_close_matches(w,data.keys())[0])).upper()
+        if yn == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+
     else:
         return "The word doesn't exist. Please double check it."
 
 word = input("Enter word: ").lower()
-print(translate(word))
+
+output = (translate(word))
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
